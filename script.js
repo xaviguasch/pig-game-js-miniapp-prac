4460266641570cf1561e9d9a1b83295d9203bddf
xaviguasch <xaviguasch@gmail.com>
2021-02-23 17:56:44 +0100

    switched the active player

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 'use strict'
 
 // Selecting elements
+const player0El = document.querySelector('.player--0')
+const player1El = document.querySelector('.player--1')
 const score0El = document.querySelector('#score--0')
 const score1El = document.querySelector('#score--1')
 const diceEl = document.querySelector('.dice')
@@ -17,7 +19,9 @@ score1El.textContent = 0
 diceEl.classList.add('hidden')
 
 // Variables
+const scores = [0, 0]
 let currentScore = 0
+let activePlayer = 0
 
 btnRoll.addEventListener('click', function () {
   // 1. Generate a random dice roll
@@ -34,9 +38,13 @@ btnRoll.addEventListener('click', function () {
   if (dice !== 1) {
     // add dice result to the current score
     currentScore += dice
-    current0El.textContent = currentScore // CHANGE LATER
+    document.querySelector(`#current--${activePlayer}`).textContent = currentScore
   } else {
-    // Switch to the next player
-    console.log('next player turn')
+    document.querySelector(`#current--${activePlayer}`).textContent = 0
+    currentScore = 0
+
+    activePlayer = activePlayer === 1 ? 0 : 1
+    player0El.classList.toggle('player--active')
+    player1El.classList.toggle('player--active')
   }
 })
