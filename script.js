d5966a9501a493b5023d7a4f8d999cb6293dd0fb
xaviguasch <xaviguasch@gmail.com>
2021-02-24 11:58:41 +0100

    added reset game option

--- script.js
+++ script.js
@@ -13,16 +13,29 @@ const btnNew = document.querySelector('.btn--new')
 const btnRoll = document.querySelector('.btn--roll')
 const btnHold = document.querySelector('.btn--hold')
 
-// Starting condition
-score0El.textContent = 0
-score1El.textContent = 0
-diceEl.classList.add('hidden')
-
 // State variables
-const scores = [0, 0]
-let currentScore = 0
-let activePlayer = 0
-let playing = true
+let scores, currentScore, activePlayer, playing
+
+// Starting conditions
+const init = function () {
+  scores = [0, 0]
+  currentScore = 0
+  activePlayer = 0
+  playing = true
+
+  score0El.textContent = 0
+  score1El.textContent = 0
+  current0El.textContent = 0
+  current1El.textContent = 0
+
+  diceEl.classList.add('hidden')
+  player0El.classList.remove('player--winner')
+  player1El.classList.remove('player--winner')
+  player0El.classList.add('player--active')
+  player1El.classList.remove('player--active')
+}
+
+init()
 
 const switchPlayer = function () {
   document.querySelector(`#current--${activePlayer}`).textContent = 0
@@ -79,3 +92,5 @@ btnHold.addEventListener('click', function () {
     }
   }
 })
+
+btnNew.addEventListener('click', init)
